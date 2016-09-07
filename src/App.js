300bba337aa1f912bfeec7b41f5d8cb373ace3c9
Sergio Andres Bedoya Garces <sbedoya@H5613.ad.hugeinc.com>
2016-09-07 16:27:46 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: '',
+      data: {}
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(e) {
+    e.preventDefault();
+
+    const value = this.inputRef.value;
+    const location = encodeURIComponent(value);
+    const urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+    const urlSuffix = '&APPID=51123fe56641db2371a40b70d56b3194&units=metric';
+    const url = urlPrefix + location + urlSuffix;
+
+    // This will only works on Chrome and Firefox
+    fetch(url).then(function(response) {
+      return response.json();
+    }).then((data) => {
+      this.setState({ data });
+    });
+  }
+
+  render() {
+    let currentTemp = 'no data';
+    const { list } = this.state.data;
+
+    if (list) {
+      currentTemp = list[0].main.temp;
+    }
+
+    return (
+      <div>
+        <h1>Welcome to React</h1>
+        <form onSubmit={this.fetchData}>
+          <label>I want to know the weather for</label>
+          <input
+            type="text"
+            placeholder={"City, Country"}
+            ref={(input) => this.inputRef = input} />
+        </form>
+        <p className="temp-wrapper">
+          <span className="temp">{ currentTemp }</span>
+          <span>°C</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
